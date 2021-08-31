c33aaaf34b450cdea4175fb9454c74aa48e458a8
santonu-ahmed <santonu974@gmail.com>
2021-08-31 14:21:11 +0600

    ede

---
+++ script.js
@@ -0,0 +1,15 @@
+const quoteText = document.getElementById('quote-text');
+
+async function getQuotes(){
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'https://goquotes-api.herokuapp.com/api/v1/random?count=1';
+    try{
+        const response = await fetch(proxyUrl + apiUrl);
+        const quotes = await response.json();
+    }
+    catch(error){
+
+    }
+}
+getQuotes()
+console.log(getQuotes)
