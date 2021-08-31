f5dc4af722232dcdea9da6e9d73bbcdcf46fd7cd
santonu-ahmed <santonu974@gmail.com>
2021-08-31 15:30:07 +0600

    changed api

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const quoteText = document.getElementById('quote-text');
 
 async function getQuotes(){
-    const apiUrl = 'https://api.chucknorris.io/jokes/random';
+    const apiUrl = ' https://sv443.net/jokeapi/v2';
     try{
         const response = await fetch(apiUrl);
         const quotes = await response.json();
